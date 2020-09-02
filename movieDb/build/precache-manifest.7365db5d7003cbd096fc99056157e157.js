9e5badc07a35d70cb68da8df473ff208097e2f45
Eugen Horbenko <hogen3107@gmail.com>
2020-09-02 10:29:17 +0300

    added project

---
+++ movieDb/build/precache-manifest.7365db5d7003cbd096fc99056157e157.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4146227231bf670adda54226718198",
+    "url": "/React_projects/index.html"
+  },
+  {
+    "revision": "b48be40aeb0e249e2153",
+    "url": "/React_projects/static/css/main.f9e681e1.chunk.css"
+  },
+  {
+    "revision": "75f3253a560197cb7bd9",
+    "url": "/React_projects/static/css/movieDetails-page.c66121c4.chunk.css"
+  },
+  {
+    "revision": "e404438f527e3533364e",
+    "url": "/React_projects/static/js/0.b5e0a838.chunk.js"
+  },
+  {
+    "revision": "1564ab1ceb11b0aab4c0",
+    "url": "/React_projects/static/js/6.fa17502b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_projects/static/js/6.fa17502b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd8631ec19d4e5692d3",
+    "url": "/React_projects/static/js/home-page.1e400841.chunk.js"
+  },
+  {
+    "revision": "b48be40aeb0e249e2153",
+    "url": "/React_projects/static/js/main.15c1c8e8.chunk.js"
+  },
+  {
+    "revision": "75f3253a560197cb7bd9",
+    "url": "/React_projects/static/js/movieDetails-page.a976bf75.chunk.js"
+  },
+  {
+    "revision": "dc93fc703c093b4a24a7",
+    "url": "/React_projects/static/js/movies-page.03dfaf32.chunk.js"
+  },
+  {
+    "revision": "4a936041fb1b875213ae",
+    "url": "/React_projects/static/js/runtime-main.90534821.js"
+  }
+]);
